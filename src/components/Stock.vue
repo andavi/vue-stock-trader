effe02eb2899a70bdefdc8ee1e884169502afba4
Andrew Davis <andrew.bradley.davis@gmail.com>
2017-07-01 20:34:11 -0400

    disable buy buttons after value reaches 0

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="col-sm-6 col-md-4">
-        <div class="panel panel-success">
+        <div class="panel" :class="[company.price > 0 ? 'panel-success' : 'panel-danger']">
             <div class="panel-heading">
 
-                    <div class="panel-title pull-left h3">
-                        {{ company.name }}
-                        <small>(Price: {{ company.price }})</small>
-                    </div>
-                    <div class="panel-title pull-right h3"><small>Shares: {{ company.quantity }}</small></div>
-                    <div class="clearfix"></div>
+                <div class="panel-title pull-left h3">
+                    {{ company.name }}
+                    <small>(Price: {{ company.price }})</small>
+                </div>
+                <div class="panel-title pull-right h3">
+                    <small>Shares: {{ company.quantity }}</small>
+                </div>
+                <div class="clearfix"></div>
             </div>
-            <div class="panel-body">
+            <div class="panel-body" v-if="company.price > 0">
                 <div class="pull-left">
-
                     <input
                             type="number"
                             class="form-control"
@@ -26,6 +27,27 @@
                             @click.prevent="buy({company: company, quantity: quantity, reset: reset})"
                             :disabled="disabled"
                     >Buy
+                    </button>
+                </div>
+            </div>
+            <div class="panel-body" v-else>
+                <div class="pull-left">
+
+                    <input
+                            id="deadinput"
+                            disabled
+                            type="number"
+                            class="form-control"
+                            placeholder="Bankrupt"
+                            v-model.number="quantity"
+                    >
+                </div>
+                <div class="pull-right">
+                    <button
+                            class="btn btn-danger"
+                            @click.prevent="buy({company: company, quantity: quantity, reset: reset})"
+                            disabled="disabled"
+                    >Closed
                     </button>
                 </div>
             </div>
